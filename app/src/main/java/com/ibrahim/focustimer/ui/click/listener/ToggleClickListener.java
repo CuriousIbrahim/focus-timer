8e850bd1ea1a9eaeb8854aea1f310f1a6a3419e8
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-05-31 19:03:51 -0400

    Fix mix up between states

--- app/src/main/java/com/ibrahim/focustimer/ui/click/listener/ToggleClickListener.java
+++ app/src/main/java/com/ibrahim/focustimer/ui/click/listener/ToggleClickListener.java
@@ -33,12 +33,14 @@ public class ToggleClickListener implements View.OnClickListener {
     }
 
     public void toggleState0() {
+        onState0 = true;
+        textView.setText(toggleText0);
+    }
+
+    public void toggleState1() {
         onState0 = false;
         textView.setText(toggleText1);
     }
 
-    public void toggleState1() {
-        onState0 = true;
-        textView.setText(toggleText0);
-    }
+
 }
