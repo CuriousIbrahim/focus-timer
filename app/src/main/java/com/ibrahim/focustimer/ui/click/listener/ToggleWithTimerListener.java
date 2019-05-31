b93cbc6794378fcfba2010c9921f15b94be6af14
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-05-31 19:52:24 -0400

    Remove ToggleClickListener and move all implementation and logic to ToggleWithTimerListener

--- app/src/main/java/com/ibrahim/focustimer/ui/click/listener/ToggleWithTimerListener.java
+++ app/src/main/java/com/ibrahim/focustimer/ui/click/listener/ToggleWithTimerListener.java
@@ -4,25 +4,51 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ibrahim.focustimer.util.ToggleSelector;
 import com.ibrahim.focustimer.util.ToggleTimer;
 
-public class ToggleWithTimerListener extends ToggleClickListener {
+public class ToggleWithTimerListener implements View.OnClickListener {
 
     ToggleTimer timer;
+    private TextView textView;
 
-    public ToggleWithTimerListener(TextView textView, String toggleText0,
-                                   String toggleText1, ToggleTimer timer) {
-        super(textView, toggleText0, toggleText1);
+    private String resumeText;
+    private String pauseText;
+
+    // State 0 = resume; State 1 = pause
+    ToggleSelector<String> toggleSelector;
+
+    public ToggleWithTimerListener(TextView textView, String resumeText,
+                                   String pauseText, ToggleTimer timer) {
+        this.textView = textView;
+
+        this.resumeText = resumeText;
+        this.pauseText = pauseText;
+
+        toggleSelector = new ToggleSelector<>(resumeText, pauseText, true);
         this.timer = timer;
     }
 
     @Override
     public void onClick(View v) {
-        if (onState0) {
-            Log.d("DEBUG", "Starting timer");
+        if (toggleSelector.isBinaryChoice0Selected()) {
             timer.resume();
-        } else
+            textView.setText(toggleSelector.toggle());
+        } else {
             timer.pause();
-        super.onClick(v);
+            textView.setText(toggleSelector.toggle());
+        }
+    }
+
+    public void setToResume() {
+        timer.resume();
+        toggleSelector.setToBinaryChoice1();
+        textView.setText(toggleSelector.getCurrentState());
+    }
+
+    public void setToPause() {
+        timer.pause();
+        toggleSelector.setToBinaryChoice0();
+        textView.setText(toggleSelector.getCurrentState());
     }
 }
