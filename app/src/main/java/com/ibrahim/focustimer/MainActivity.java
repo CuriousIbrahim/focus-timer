d47891fdc6b2f02fadd7d4a38fdb0fd0cc8c7b1f
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-05-31 15:51:46 -0400

    Add constants and instance variables

--- app/src/main/java/com/ibrahim/focustimer/MainActivity.java
+++ app/src/main/java/com/ibrahim/focustimer/MainActivity.java
@@ -2,8 +2,23 @@ package com.ibrahim.focustimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ibrahim.focustimer.ui.click.listener.ToggleWithTimerListener;
+import com.ibrahim.focustimer.util.ToggleTimer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long DEFAULT_WORK_TIME = 25 * 60 * 1000;
+    private static final long DEFAULT_SHORT_REST_TIME = 5 * 60 * 1000;
+    private static final long DEFAULT_LONG_REST_TIME = 30 * 60 * 1000;
+
+    TextView timeTextView;
+    Button startAndStopToggle;
+
+    ToggleTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
