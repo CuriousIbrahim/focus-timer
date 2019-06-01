30616af8cf4eedf3ad0f56709433bfa3173361a8
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-06-01 09:23:46 -0400

    Create PomodoroCycle intended to implement Pomodoro Technique (work, short break, long break - cycles)

---
+++ app/src/main/java/com/ibrahim/focustimer/PomodoroCycle.java
@@ -0,0 +1,45 @@
+package com.ibrahim.focustimer;
+
+public class PomodoroCycle {
+
+    private static final int WORK_STATE = 0;
+    private static final int SHORT_BREAK_STATE = 1;
+    private static final int LONG_BREAK_STATE = 2;
+
+    private int currentState;
+
+    private int workCount;
+
+    public PomodoroCycle() {
+        currentState = WORK_STATE;
+        workCount = 0;
+    }
+
+    public void next() {
+        if (currentState == WORK_STATE) {
+            if (workCount > 4) {
+                workCount = 0;
+                currentState = LONG_BREAK_STATE;
+            } else {
+                currentState = SHORT_BREAK_STATE;
+            }
+        } else if (currentState == SHORT_BREAK_STATE) {
+            workCount++;
+            currentState = WORK_STATE;
+        } else {
+            currentState = LONG_BREAK_STATE;
+        }
+    }
+
+    public boolean isWorkState() {
+        return currentState == WORK_STATE;
+    }
+
+    public boolean isShortBreakState() {
+        return currentState == SHORT_BREAK_STATE;
+    }
+
+    public boolean isLongBreakState() {
+        return currentState == LONG_BREAK_STATE;
+    }
+}
