955ddc1c4e92561fa2aacfab2255774f5f4a7348
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-06-03 01:25:22 -0400

    Implement MainActivityViewModel

--- app/src/main/java/com/ibrahim/focustimer/MainActivity.java
+++ app/src/main/java/com/ibrahim/focustimer/MainActivity.java
@@ -1,27 +1,20 @@
 package com.ibrahim.focustimer;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.ibrahim.focustimer.ui.click.listener.ToggleWithTimerListener;
-import com.ibrahim.focustimer.util.Time;
-import com.ibrahim.focustimer.ui.util.TimerTextViewToggle;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final long DEFAULT_WORK_TIME = 25 * 60 * 1000;
-    private static final long DEFAULT_SHORT_REST_TIME = 5 * 60 * 1000;
-    private static final long DEFAULT_LONG_REST_TIME = 30 * 60 * 1000;
 
     TextView timeTextView;
 
     Button startAndStopToggle;
     Button stop;
 
-    TimerTextViewToggle timer;
+    MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +23,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timeTextView = findViewById(R.id.time);
-
         startAndStopToggle = findViewById(R.id.startAndStopToggle);
         stop = findViewById(R.id.stop);
 
-        timer = new TimerTextViewToggle(DEFAULT_WORK_TIME, 1000, timeTextView);
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
-        final View.OnClickListener resumeAndPauseToggleListener = new ToggleWithTimerListener(
-                startAndStopToggle, "Resume", "Pause", timer);
-        startAndStopToggle.setOnClickListener(resumeAndPauseToggleListener);
+        viewModel.getTimeLeft().observe(this, timeLeft -> {
+            Log.d("MainActivity", String.format("Time left: %s", timeLeft));
+            timeTextView.setText(timeLeft);
+        });
 
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timer.stop();
-                timer.setNewTime(DEFAULT_WORK_TIME);
-                timeTextView.setText(Time.millisToString(DEFAULT_WORK_TIME));
-                ((ToggleWithTimerListener) resumeAndPauseToggleListener).setToPause();
+        viewModel.getOnResume().observe(this, (onResume) -> {
+            if (onResume) {
+                startAndStopToggle.setText("Pause");
+            } else {
+                startAndStopToggle.setText("Resume");
             }
         });
+
+        startAndStopToggle.setOnClickListener((a) -> {
+            viewModel.toggleTime();
+        });
+
+        stop.setOnClickListener((a) -> {
+            viewModel.stopTime();
+        });
     }
-
-
 }
