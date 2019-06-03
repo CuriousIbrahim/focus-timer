790657b229db599825e84b38936702e55da8eced
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-06-03 04:46:46 -0400

    Add constant - how many milliseconds in one second

--- app/src/main/java/com/ibrahim/focustimer/C.java
+++ app/src/main/java/com/ibrahim/focustimer/C.java
@@ -6,5 +6,7 @@ public class C {
     public static final long DEFAULT_SHORT_REST_TIME = 5 * 60 * 1000;
     public static final long DEFAULT_LONG_REST_TIME = 30 * 60 * 1000;
 
+    public static final long SECOND = 1000;
+
 
 }
