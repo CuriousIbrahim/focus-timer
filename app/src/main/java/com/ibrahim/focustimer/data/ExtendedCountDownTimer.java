55a431abc4df1223cfe807bb95238b077cd97754
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-06-02 23:56:07 -0400

    Create an extended version of CountDownTime that allows for pause and resume

---
+++ app/src/main/java/com/ibrahim/focustimer/data/ExtendedCountDownTimer.java
@@ -0,0 +1,54 @@
+package com.ibrahim.focustimer.data;
+
+import android.os.CountDownTimer;
+
+public abstract class ExtendedCountDownTimer {
+
+    private CountDownTimer countDownTimer;
+
+    private long millisLeft;
+    private long countDownInterval;
+
+
+    public ExtendedCountDownTimer(long millisInFuture, long countDownInterval) {
+        countDownTimer = new MyCountDownTimer(millisInFuture, countDownInterval);
+        millisLeft = millisInFuture;
+        this.countDownInterval = countDownInterval;
+    }
+
+    public void start() {
+        countDownTimer.start();
+    }
+
+    public void resume() {
+        countDownTimer = new MyCountDownTimer(millisLeft, countDownInterval);
+        countDownTimer.start();
+    }
+
+    public void pause() {
+        countDownTimer.cancel();
+    }
+
+    abstract void onTick(long millisUntilFinished);
+
+    abstract void onFinish();
+
+
+    class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            millisLeft = millisUntilFinished;
+            onTick(millisUntilFinished);
+        }
+
+        @Override
+        public void onFinish() {
+            onFinish();
+        }
+    }
+}
