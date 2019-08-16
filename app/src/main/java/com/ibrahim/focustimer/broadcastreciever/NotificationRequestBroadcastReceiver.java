e9baa91f6592f9a8dc29a4d98abf27e26766c1f1
Ibrahim Abdul Hammed <ibrahim74115@gmail.com>
2019-08-16 00:36:04 -0400

    Remove unused lines of code

--- app/src/main/java/com/ibrahim/focustimer/broadcastreciever/NotificationRequestBroadcastReceiver.java
+++ app/src/main/java/com/ibrahim/focustimer/broadcastreciever/NotificationRequestBroadcastReceiver.java
@@ -3,13 +3,10 @@ package com.ibrahim.focustimer.broadcastreciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.ibrahim.focustimer.notification.NotificationManager;
 
 public class NotificationRequestBroadcastReceiver extends BroadcastReceiver {
-
-    private static final String TAG  = "NotificationRequestBR";
 
     @Override
     public void onReceive(Context context, Intent intent) {
