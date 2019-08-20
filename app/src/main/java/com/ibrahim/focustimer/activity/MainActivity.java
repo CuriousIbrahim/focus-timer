3856e7e40c3b2933e609f32af697803da2cab131
Ibrahim Abdul Hammed <ibrahim74115@gmail.com>
2019-08-20 00:33:27 -0400

    Add menu that takes you to settings activity

--- app/src/main/java/com/ibrahim/focustimer/activity/MainActivity.java
+++ app/src/main/java/com/ibrahim/focustimer/activity/MainActivity.java
@@ -2,11 +2,15 @@ package com.ibrahim.focustimer.activity;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
     private Button next;
 
     private MainActivityViewModel viewModel;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.button_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
