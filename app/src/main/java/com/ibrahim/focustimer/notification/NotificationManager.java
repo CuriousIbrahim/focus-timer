e1551ded6774eb291f47d0760afe270b5c677167
Ibrahim Abdul Hammed <ibrahim74115@gmail.com>
2019-08-24 18:04:07 -0400

    Change icons for launcher, app, and notification

--- app/src/main/java/com/ibrahim/focustimer/notification/NotificationManager.java
+++ app/src/main/java/com/ibrahim/focustimer/notification/NotificationManager.java
@@ -49,7 +49,7 @@ public class NotificationManager {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentTitle("Focus Timer")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setSmallIcon(R.drawable.work_icon_background);
+                .setSmallIcon(R.drawable.work_notification_icon);
 
         return builder;
     }
